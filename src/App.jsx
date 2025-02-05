2384360e4005ca75c3d43b4126336b9f819d937c
Septian Hadi Nugroho <septianhadinugroho4@gmail.com>
2025-02-05 10:38:49 +0700

    make a game board

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import { useState } from 'react'
 
-function App() {
+function Square() {
+  const [value, setValue] = useState('')
+
+  function handleClick() {
+    setValue('X');
+  }
+
   return (
-    <>
-    </>
-  )
+    <button className='square' onClick={handleClick}>
+      {value}
+    </button>
+  );
 }
 
-export default App
+export default function Board() {
+  return (
+    <div className='board'>
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+    </div>
+  );
+}
